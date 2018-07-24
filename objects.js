c8c94fdcc8f1dbd424f5553bab8d9c1eca84523e
Hanna Strezh <hannastrezh@gmail.com>
2018-07-24 17:49:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,1 @@
-var playlist ={
-  Queen: "Champions",
-  LadyGaga: "Heloo",
-  Prince:"Goodbye"
-};
-function updatePlaylist(list, artist, song){
-  list[artist]=song
-  return list;
-}
-function removeFromPlaylist (list, name){
-  delete list.name;
-  return list;
-}
+objects lab 1
